eb40c9940eaeb688919b4278bab2be3da73cbc48
Evan Von Oehsen <evanvono@gmail.com>
2022-04-05 21:07:41 -0700

    camera now properly rotates with mouse movement

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,45 @@
 #![allow(dead_code)]
 
 use color_eyre::eyre::Result;
-use winit::event::VirtualKeyCode;
 use std::sync::{Arc, Mutex};
+use winit::event::VirtualKeyCode;
 
+mod camera;
 mod engine;
 mod image;
 mod input;
-mod types;
-mod camera;
-mod vulkan;
 mod renderer;
+mod types;
+mod vulkan;
 use types::*;
 
-const DT:f32 = 1.0/60.0;
-
+const DT: f32 = 1.0 / 60.0;
 
 #[derive(Debug)]
 struct GenericGameThing {}
-impl engine::GameThing for GenericGameThing {
-}
+impl engine::GameThing for GenericGameThing {}
 
 fn main() -> Result<()> {
     color_eyre::install()?;
 
     // If GameThing variants differ widely in size, consider using
     // Box<GameThing>
-    let mut engine:engine::Engine = engine::Engine::new(engine::WindowSettings::default());
+    let mut engine: engine::Engine = engine::Engine::new(engine::WindowSettings::default());
 
-    engine.set_camera(camera::Camera::look_at(Vec3::new(0.,-2.,-10.), Vec3::zero(), Vec3::unit_y()));
+    engine.set_camera(camera::Camera::look_at(
+        Vec3::new(0., -2., -10.),
+        Vec3::zero(),
+        Vec3::unit_y(),
+    ));
     let tex = engine.load_texture(std::path::Path::new("content/robot.png"))?;
-    let mesh = engine.load_mesh(std::path::Path::new("content/characterSmall.fbx"),0.1)?;
+    let mesh = engine.load_mesh(std::path::Path::new("content/characterSmall.fbx"), 0.1)?;
     let model = engine.create_model(&mesh, &tex);
 
     engine.create_game_object(
         Some(&model),
-        Isometry3::new(
-            Vec3::new(0.0, -12.5, 25.0),
-            Rotor3::identity()
-        ),
-        Box::new(GenericGameThing{}),
-        None
+        Isometry3::new(Vec3::new(0.0, -12.5, 25.0), Rotor3::identity()),
+        Box::new(GenericGameThing {}),
+        None,
     );
 
     let mut cam_x = Arc::new(Mutex::new(0.));
@@ -56,8 +55,6 @@ fn main() -> Result<()> {
         let mut cam_degrees_y_lock = cam_degrees_y.lock().unwrap();
         let mut cam_degrees_x_lock = cam_degrees_x.lock().unwrap();
         let mut cam_to_z_lock = cam_to_z.lock().unwrap();
-        
-        
         let input = _engine.get_inputs();
 
         if input.is_key_down(VirtualKeyCode::Up) {
@@ -82,11 +79,12 @@ fn main() -> Result<()> {
         *cam_degrees_y_lock -= (x_diff / mouse_move_scale) as f32;
         *cam_degrees_x_lock -= (y_diff / mouse_move_scale) as f32;
 
-        let cam_eye = Vec3::new(*cam_x_lock,*cam_y_lock,-10.);
-        // let cam_to = Vec3::new(*cam_x_lock + *cam_to_x_lock,*cam_y_lock + *cam_to_y_lock, *cam_to_z_lock);
-        let camera = camera::Camera::look_at_degrees(cam_eye, Vec3::unit_y(), (*cam_degrees_y_lock, *cam_degrees_x_lock));
-        // let camera = camera::Camera::look_at(cam_eye, cam_to, Vec3::unit_y());
+        let cam_eye = Vec3::new(*cam_x_lock, *cam_y_lock, -10.);
+        let camera = camera::Camera::look_at_degrees(
+            cam_eye,
+            Vec3::unit_y(),
+            (*cam_degrees_y_lock, *cam_degrees_x_lock),
+        );
         _engine.set_camera(camera);
-
     })
 }
